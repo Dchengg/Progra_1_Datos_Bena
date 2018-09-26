9d0a8392667a415cc5530865211389c9affc9d70
Justin Bogantes <justin.bogantes99@gmail.com>
2018-09-26 11:58:28 -0600

    pila

--- pila.cpp
+++ pila.cpp
@@ -1,1 +1,12 @@
+#include "pila.h"
+#include <iostream>
+#include <stdio.h>
 
+using namespace std;
+
+template<class T>
+pila<T>::pila()
+{
+    Tope = 0;
+    Fondo = 0;
+}
--- pila.h
+++ pila.h
@@ -1,4 +1,17 @@
 #ifndef PILA_H
 #define PILA_H
+#include "Nodo.h"
+
+template<class T>
+class pila
+{
+private:
+    Nodo<T>* Tope;
+    Nodo<T>* Fondo;
+public:
+    pila();
+    void push(T &x);
+    void imprimirPila();
+};
 
 #endif // PILA_H
